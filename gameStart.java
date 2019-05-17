a60a9e46ac6424e15534ec024011c7daa6b11e90
Amini-Kisomi <335540290@EDU.YRDSB.NET>
2019-05-17 11:27:50 -0400

    added menu so the game starts from menu instead of ur game

--- gameStart.java
+++ gameStart.java
@@ -1,5 +1,5 @@
 class gameStart {
     public static void main(String[] args) {
-        Game game = new Game();
+        Menu game = new Menu();
     }
 }
---
+++ gameStart.java~
@@ -0,0 +1,5 @@
+class gameStart {
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+}
