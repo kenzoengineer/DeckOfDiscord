4ec85698b2a0f4803a1df7f699f337ca14fe05db
Ken Jiang <kenzoengineer@gmail.com>
2019-06-10 10:55:44 -0400

    attacking ACTUALLY WORKS

--- gameStart.java
+++ gameStart.java
@@ -1,7 +1,7 @@
 class gameStart {
   public static void main(String[] args) {
-    Menu game = new Menu();
-    //Game game = new Game(1);
+    //Menu game = new Menu();
+    Game game = new Game(1);
     //Entity game = new Entity();
   }
 }
