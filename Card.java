84bb9e4b9f2dc7cd5f242685d39690fc5f5ab0ca
Ken Jiang <073686289@gapps.yrdsb.ca>
2019-06-12 20:14:42 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,3 +1,14 @@
+/**
+ * Abstract class to hold a card
+ * object, used for compatibility
+ * in case special cards want to
+ * be added
+ * 
+ * @author Souren A., Ken J.
+ * @since May 26th, 2019
+ * @version 2.49
+ */
+
 abstract class Card {
     private String name;
     private String description;
@@ -5,9 +16,19 @@ abstract class Card {
         name = n;
         description = d;
     }
+    
+    /**
+     * Gets name
+     * @return name
+     */
     public String getName() {
         return name;
     }
+    
+    /**
+     * Gets image
+     * @return image
+     */
     public String getDes() {
         return description;
     }
